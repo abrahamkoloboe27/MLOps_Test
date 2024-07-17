7801ee5f0bc4d8748f823985deb24b9ec3dbeee4
Abraham KOLOBOE <abrahamkoloboe2000@gmail.com>
2024-07-17 18:27:42 +0100

    `chore: Update requirements.txt with pycaret library`

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,8 +1,7 @@
 # scripts/train_model.py
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from pycaret.regression import *
 import joblib
 
 # Chemin vers votre fichier de données
@@ -11,23 +10,36 @@ data_path = 'data/Housing.csv' # Remplacer 'data_path' par le chemin de votre fi
 # Charger les données
 df = pd.read_csv(data_path)
 
-# Prétraitement des données (exemple : séparation des features et de la cible)
-X = df.drop('price', axis=1)  # Remplacer 'target_column_name' par le nom de votre colonne cible
-y = df['price']
+# Initialiser l'environnement PyCaret
+s = setup(df, 
+          target = 'price', 
+          session_id = 123, 
+          train_size=0.7,
+          fold_strategy="kfold",
+          fold=10,
+          numeric_features = ['area', 'bedrooms', 'bathrooms', 'stories', 'parking'],
+          categorical_features = ['mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'prefarea', 'furnishingstatus'],
+          profile = True
+          )
 
-# Division des données en ensemble d'entraînement et ensemble de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Comparer les modèles
+best_model = compare_models(sort="rmse", n_select=3)
+best_model
 
-# Initialisation du modèle (exemple : Régression Linéaire)
-model = LinearRegression()
+# Highlight the minimum value of each column
+highlight_min = lambda x: ['background-color: yellow' if v == x.min() else '' for v in x]
+pull().style.apply(highlight_min, axis=0)
 
-# Entraînement du modèle
-model.fit(X_train, y_train)
+# Fine-tune the best model
+tuned_models = [tune_model(model) for model in best_model]
+tuned_models
 
-# Chemin pour sauvegarder le modèle
-model_path = 'housing_price_prediction_model.pkl'
 
-# Sauvegarder le modèle formé
-joblib.dump(model, model_path)
+# Compare the tuned models
+best_model_tuned = compare_models(tuned_models, sort="rmse", n_select=3)
 
-print("Entraînement du modèle terminé et modèle sauvegardé avec succès.")
+
+#Finalize the best model
+final_model = finalize_model(best_model_tuned[0])
+
+save_model(final_model, 'models/housing_price_prediction_model')
