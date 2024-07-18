4962b2427173e527cbcf3f0afdce1dafdc016f3d
Abraham KOLOBOE <abrahamkoloboe2000@gmail.com>
2024-07-18 11:16:37 +0100

    chore: Update train_model.py to use pycaret.regression.create_model instead of pycaret.regression.*

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,7 +1,7 @@
 # scripts/train_model.py
 
 import pandas as pd
-from pycaret.regression import *
+from pycaret.regression import create_model, setup, save_model
 import joblib
 
 
@@ -25,23 +25,24 @@ s = setup(df,
           )
 
 # Comparer les modèles
-best_model = compare_models(["ridge", "rf","knn","lasso","lr"],sort="rmse", n_select=3)
-best_model
+
+#best_model = compare_models(["ridge", "rf","knn","lasso","lr"],sort="rmse", n_select=3)
+best_model = create_model("lr")
 
 # Highlight the minimum value of each column
-highlight_min = lambda x: ['background-color: yellow' if v == x.min() else '' for v in x]
-pull().style.apply(highlight_min, axis=0)
+#highlight_min = lambda x: ['background-color: yellow' if v == x.min() else '' for v in x]
+#pull().style.apply(highlight_min, axis=0)
 
 # Fine-tune the best model
-tuned_models = [tune_model(model) for model in best_model]
-tuned_models
+#tuned_models = [tune_model(model) for model in best_model]
+#tuned_models
 
 
 # Compare the tuned models
-best_model_tuned = compare_models(tuned_models, sort="rmse", n_select=3)
+#best_model_tuned = compare_models(tuned_models, sort="rmse", n_select=3)
 
 
 #Finalize the best model
-final_model = finalize_model(best_model_tuned[0])
-
-save_model(final_model, 'housing_price_prediction_model')
+#final_model = finalize_model(best_model_tuned[0])
+save_model(best_model, 'housing_price_prediction_model')
+#save_model(final_model, 'housing_price_prediction_model')
