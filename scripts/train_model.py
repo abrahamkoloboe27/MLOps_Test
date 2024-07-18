73a630b0b621f3fb1da5bea52ddd5e74a761dc27
Abraham KOLOBOE <abrahamkoloboe2000@gmail.com>
2024-07-18 12:12:38 +0100

    `chore: Update Dockerfile path in train_model.yml and print current path in train_model.py`

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -50,5 +50,5 @@ save_model(best_model, 'housing_price_prediction_model')
 # Obetenez le path actuel
 import os
 current_path = os.getcwd()
-
+print(current_path)
 joblib.dump(best_model, os.path.join(current_path, 'housing_price_prediction_model.pkl'))
